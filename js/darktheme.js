929924da54b8cbc471fe4c91f1053fff6943ca23
George <theprosto00@mail.ru>
2019-11-09 14:41:29 +0300

    init commit

--- js/darktheme.js
+++ js/darktheme.js
@@ -1,27 +1,25 @@
-document.getElementById('checkbox1').addEventListener('click', function() {
+document.getElementById('checkbox').addEventListener('click', function() {
     if (!night) {
         document.body.style.background='linear-gradient(to top, #262725, DimGray)'
         document.getElementById('prog').style.background='#737373'
         document.getElementById('prog').style.transition='0.5s'
         document.getElementById('prog').style.color='white'
-        document.getElementById('prog').style.transition='0.5s'            
+        document.getElementById('prog').style.transition='0.5s'
         document.getElementById('news').style.background='#737373'
         document.getElementById('news').style.color='white'
-        document.getElementById('news').style.transition='0.5s'            
+        document.getElementById('news').style.transition='0.5s'
         document.getElementById('news_l').style.background='#737373'
         document.getElementById('news_l').style.color='white'
-        document.getElementById('news_l').style.transition='0.5s'            
+        document.getElementById('news_l').style.transition='0.5s'
         document.getElementById('news_w').style.background='#737373'
         document.getElementById('news_w').style.color='white'
-        document.getElementById('news_w').style.transition='0.5s'            
+        document.getElementById('news_w').style.transition='0.5s'
         document.getElementById('news_q').style.background='#737373'
         document.getElementById('news_q').style.color='white'
-        document.getElementById('news_q').style.transition='0.5s'           
+        document.getElementById('news_q').style.transition='0.5s'
         document.getElementById('news_aga').style.background='#737373'
-        document.getElementById('top').style.background='linear-gradient(to top, #999999, #999999)'
-        document.getElementById('top').style.transition='0.5s' 
         document.getElementById('chat').style.background='#737373'
-        document.getElementById('chat').style.transition='0.5s'          
+        document.getElementById('chat').style.transition='0.5s'
         night = true
     }
     else {
@@ -39,7 +37,6 @@ document.getElementById('checkbox1').addEventListener('click', function() {
         document.getElementById('news_aga').style.background='white'
         document.getElementById('chat').style.background='#e4e4e4'
         document.getElementById('chat').style.transition='0.5s'
-        document.getElementById('top').style.background='linear-gradient(to top, white, white)'
         night = false
     }
 })
